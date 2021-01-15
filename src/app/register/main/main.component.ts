da223c5804074f5869b2f8dc61657d57140f702f
Fredrik Thelander <fredrikthelander@outlook.com>
2021-01-15 10:50:44 +0100

    ft

--- src/app/register/main/main.component.ts
+++ src/app/register/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService, AppInfoService, DbService, MsgBusService } from '../../shared/services';
+
+import { Socket } from 'ngx-socket-io'
 
 @Component({
   selector: 'app-main',
@@ -7,6 +10,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  codeChars = '23456789ABCEFGHJKMNPQRSTWXYZ'
+
   vars = {
     name: '',
     persons: null,
@@ -14,10 +19,23 @@ export class MainComponent implements OnInit {
     email: '',
     phone: '',
     catalogs: false,
-    newsletter: false
+    catalogAdr1: '',
+    catalogAdr2: '',
+    catalogAdr3: '',
+    newsletter: true,
+    code: '',
+    password: ''
   }
 
-  constructor() { }
+  constructor(private authService: AuthService, private db: DbService, private socket: Socket) {
+
+    this.vars.code = this.createCode()
+    this.vars.password = this.createCode()
+
+    this.socket.emit('login', { token: this.db.token, system: 'grillkol', username: 'web', password: 'web' }, (result) => {
+      console.log(result)
+    })
+  }
 
   ngOnInit() {
   }
@@ -26,8 +44,20 @@ export class MainComponent implements OnInit {
     
   }
 
-  catalogClick(e) {
-    console.log(e)
+  createCode(codeLength = 6) {
+
+    // Create code
+    var s = ''
+    for (var n = 1; n <= codeLength; n++) {
+      s += this.codeChars[this.getRndInteger(0, this.codeChars.length - 1)]
+    }
+
+    return s
+    
+  }
+
+  getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
   }
 
 }
