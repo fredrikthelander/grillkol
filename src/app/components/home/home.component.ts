c0d2041b5d79ffacda7ac16c1074aa16ef7019e2
Fredrik Thelander <fredrikthelander@outlook.com>
2021-01-21 08:47:13 +0100

    ft

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DbService, AuthService } from '../../shared/services'
+import { Project } from '../../interfaces/project'
 
 @Component({
   selector: 'app-home',
@@ -10,10 +11,19 @@ export class HomeComponent implements OnInit {
 
   homeText = ''
 
+  projects: Project[] = []
+
   constructor(public db: DbService, public auth: AuthService) { 
+
     this.db.getStringSetting('hometext').then(result => {
       this.homeText = result
     })
+
+    this.db.sendMessagePromise('mget', { system: this.auth.system, table: 'projects', token: this.db.token, condition: { email: this.auth.username, active: true }, sort: { ts: 1 } }).then((result: any) => {
+      this.projects = result.data
+    })
+
+
   }
 
   ngOnInit() {
