a91f7df355a7cab4f223bb6c5b02ed3bc475e242
Fredrik Thelander <fredrikthelander@outlook.com>
2021-03-11 08:51:43 +0100

    ft

--- src/app/reports/main/main.component.ts
+++ src/app/reports/main/main.component.ts
@@ -14,6 +14,7 @@ export class MainComponent implements OnInit {
   //projects: Project[] = []
   project: Project
   orders: Order[] = []
+  orderList = []
 
   constructor(public auth: AuthService, private db: DbService, private router: Router) {}
 
@@ -23,6 +24,34 @@ export class MainComponent implements OnInit {
   async setup() {
 
     this.orders = await <any>this.db.sendMessagePromiseData('mget', { system: this.auth.system, table: 'orders', token: this.db.token, condition: { "project.id": this.project.id }, sort: { } })
+
+    this.orderList = []
+
+    this.orders.forEach(o => {
+      
+      o.items.forEach(oi => {
+
+        let oli = this.orderList.find(oli => oli.idProduct == oi.product.id)
+
+        if (oli) {
+
+          oli.quantity += oi.quantity
+          oli.amount += oi.total
+
+        } else {
+
+          this.orderList.push({
+            idProduct: oi.product.id,
+            name: oi.product.name,
+            quantity: oi.quantity,
+            amount: oi.total
+          })
+
+        }
+
+      })
+
+    })
 
   }
 
