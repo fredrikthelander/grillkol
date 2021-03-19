fb4f84dbb29f67e368126eb7a50cd812075d622b
Fredrik Thelander <fredrikthelander@outlook.com>
2021-03-19 08:28:58 +0100

    ft

--- src/app/orders/order-list/order-list.component.ts
+++ src/app/orders/order-list/order-list.component.ts
@@ -11,13 +11,50 @@ export class OrderListComponent implements OnInit {
 
   dataSource: any = {}
 
+  orders: Order[] = []
+  orderList = []
+
   constructor(public db: DbService, private auth: AuthService) {
 
     if (this.auth.userlevel > 1) this.db.createDataSource(this.auth.system, 'orders', this.dataSource)
-
+    this.setup()
   }
 
   ngOnInit() {
+  }
+
+  async setup() {
+
+    this.orders = await <any>this.db.sendMessagePromiseData('mget', { system: this.auth.system, table: 'orders', token: this.db.token, condition: {  }, sort: { } })
+
+    this.orderList = []
+
+    this.orders.forEach(o => {
+      
+      o.items.forEach(oi => {
+
+        let oli = this.orderList.find(oli => oli.idProduct == oi.product.id)
+
+        if (oli) {
+
+          oli.quantity += oi.quantity
+          oli.amount += oi.total
+
+        } else {
+
+          this.orderList.push({
+            idProduct: oi.product.id,
+            name: oi.product.name,
+            quantity: oi.quantity,
+            amount: oi.total
+          })
+
+        }
+
+      })
+
+    })
+
   }
 
 }
